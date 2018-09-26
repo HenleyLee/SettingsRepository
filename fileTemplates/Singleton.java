f29ddf6c4e1224fb27062160dde03d4bdd8dc93e
Yunlong Li <lyl873825813@sina.com>
2018-09-26 10:06:20 +0800

    Update Singleton.java

--- fileTemplates/Singleton.java
+++ fileTemplates/Singleton.java
@@ -5,17 +5,17 @@
 #parse("File Header.java")
 #if (${VISIBILITY} == "PUBLIC")public #end class ${NAME} #if (${SUPERCLASS} != "")extends ${SUPERCLASS} #end #if (${INTERFACES} != "")implements ${INTERFACES} #end {
     
-    private static volatile ${NAME} sInstance;
+    private volatile static ${NAME} INSTANCE;
 
     #if (${VISIBILITY} == "PUBLIC")public #end static ${NAME} getInstance() {
-        if (sInstance == null) {
+        if (INSTANCE == null) {
             synchronized (${NAME}.class) {
-                if (sInstance == null) {
-                    sInstance = new ${NAME}();
+                if (INSTANCE == null) {
+                    INSTANCE = new ${NAME}();
                 }
             }
         }
-        return sInstance;
+        return INSTANCE;
     }
 
     private ${NAME}() {
